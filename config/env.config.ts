a6684a7c125693dfa36fefd31b89bad74a627258
bikash-mazumdeR <bikash.htc@gmail.com>
2026-05-22 18:56:45 +0530

    Change default values of environment variables
    Updated environment variables to default to empty strings.

--- config/env.config.ts
+++ config/env.config.ts
@@ -8,11 +8,11 @@ dotenv.config({ path: envPath });
 
 export const env = {
   NODE_ENV: ENV,
-  BASE_URL: process.env.BASE_URL || 'https://www.saucedemo.com/',
-  API_URL: process.env.API_URL || 'https://restful-booker.herokuapp.com',
-  API_USER: process.env.API_USER || 'admin',
-  API_PASS: process.env.API_PASS || 'password123',
-  UI_USER: process.env.UI_USER || 'standard_user',
-  UI_PASS: process.env.UI_PASS || 'secret_sauce',
+  BASE_URL: process.env.BASE_URL || '',
+  API_URL: process.env.API_URL || '',
+  API_USER: process.env.API_USER || '',
+  API_PASS: process.env.API_PASS || '',
+  UI_USER: process.env.UI_USER || '',
+  UI_PASS: process.env.UI_PASS || '',
   LOG_LEVEL: process.env.LOG_LEVEL || 'info',
 };
